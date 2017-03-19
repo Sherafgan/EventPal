5edb41cfdc62f25acd90ccbddcd04f1afc8d4ad1
guediagael <e.guedia@innopolis.ru>
2017-03-19 08:40:03 +0300

    Android event creation done #EP-15 Fixed

---
+++ Android/app/src/main/java/android/eventpal/EventCreation.java
@@ -0,0 +1,13 @@
+package android.eventpal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_creation);
+    }
+}
